b645f2216f145869eea9636cfc82c0f1e6aa22dc
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-12-01 13:54:30 +0100

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine($"Day 1{Day1()}");
+
+
+
+static string Day1()
+{
+    string[] list = File.ReadAllLines(@"C:\Users\joaki\source\repos\AdventOfCode2022\AdventOfCode2022\Files\list.txt");
+    Dictionary<int, int> dict = new();
+    int key = 1;
+    foreach (string line in list)
+    {
+        if (string.IsNullOrEmpty(line))
+        {
+            key++;
+            continue;
+        }
+        else if (!dict.ContainsKey(key))
+        {
+            dict.Add(key, 0);
+            dict[key] = int.Parse(line);
+            continue;
+        }
+        else
+            dict[key] += int.Parse(line);
+    }
+    return $"\nPart 1: {dict.Select(x => x.Value).Max()}" +
+           $"\nPart 2: {dict.OrderByDescending(x => x.Value).Take(3).Sum(x => x.Value)}";
+}
