94e96782d43eb45a39b668ea28122c754bb8cf5f
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-12-01 14:02:02 +0100

    Day1

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -6,22 +6,22 @@ static string Day1()
 {
     string[] list = File.ReadAllLines(@"Files/list.txt");
     Dictionary<int, int> dict = new();
-    int key = 1;
+    int elf = 1;
     for (int i = 0; i < list.Length; i++)
     {
         if (string.IsNullOrEmpty(list[i]))
         {
-            key++;
+            elf++;
             continue;
         }
-        else if (!dict.ContainsKey(key))
+        else if (!dict.ContainsKey(elf))
         {
-            dict.Add(key, 0);
-            dict[key] = int.Parse(list[i]);
+            dict.Add(elf, 0);
+            dict[elf] = int.Parse(list[i]);
             continue;
         }
         else
-            dict[key] += int.Parse(list[i]);
+            dict[elf] += int.Parse(list[i]);
     }
     return $"\nPart 1: {dict.Select(x => x.Value).Max()}" +
            $"\nPart 2: {dict.OrderByDescending(x => x.Value).Take(3).Sum(x => x.Value)}";
