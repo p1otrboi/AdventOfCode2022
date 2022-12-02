bdb187464e1a511d9c3c509b3a918626f46052f7
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-12-03 00:32:44 +0100

    Day2

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.WriteLine($"Day 1{Day1()}");
+Day2();
 
 
 
@@ -25,4 +26,27 @@ static string Day1()
     }
     return $"\nPart 1: {dict.Select(x => x.Value).Max()}" +
            $"\nPart 2: {dict.OrderByDescending(x => x.Value).Take(3).Sum(x => x.Value)}";
+}
+
+static void Day2()
+{
+    var dict = new Dictionary<char, int>() { 
+        { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 }, 
+        { 'A', 1 }, { 'B', 2 }, { 'C', 3 } };
+
+    int[] rps = new int[] { 0, 1, 2, };
+
+    int score = 0;
+
+    string[] strategy = File.ReadAllLines(@"Files/day2.txt");
+
+    for (int i = 0; i <= strategy.Length; i++)
+    {
+        int decision = ((dict[strategy[i].ToArray()[2]])-(dict[strategy[i].ToArray()[2]])+3)% 3;
+        Console.WriteLine(decision);
+    }
+
+
+
+
 }
