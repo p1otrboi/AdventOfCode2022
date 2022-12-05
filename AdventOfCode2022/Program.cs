faef34443b996a08608d711b5c50712b8b35123b
Joakim Lindgren <102698651+p1otrboi@users.noreply.github.com>
2022-12-05 11:20:07 +0100

    day 4

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -138,3 +138,39 @@ using System.Text.RegularExpressions;
 //    }
 //    Console.WriteLine(keysumtotal);
 //}
+
+//static void Day4part1()
+//{
+//    string[] input = File.ReadAllLines(@"Files/day4.txt");
+//    int count = 0;
+//    foreach (string line in input)
+//    {
+//        int elf1low = int.Parse(line.Split('-',',')[0]);
+//        int elf1hi = int.Parse(line.Split('-', ',')[1]);
+//        int elf2low = int.Parse(line.Split('-', ',')[2]);
+//        int elf2hi = int.Parse(line.Split('-', ',')[3]);
+
+//        if ((elf1low <= elf2low && elf1hi >= elf2hi) || (elf2low <= elf1low && elf2hi >= elf1hi))
+//            count++;
+//    }
+//    Console.WriteLine(count);
+//}
+
+//static void Day4part2()
+//{
+//    string[] input = File.ReadAllLines(@"Files/day4.txt");
+//    int count = 0;
+//    foreach (string line in input)
+//    {
+//        int elf1low = int.Parse(line.Split('-', ',')[0]);
+//        int elf1hi = int.Parse(line.Split('-', ',')[1]);
+//        var elfone = Enumerable.Range(elf1low, (elf1hi - elf1low)+1).ToArray();
+//        int elf2low = int.Parse(line.Split('-', ',')[2]);
+//        int elf2hi = int.Parse(line.Split('-', ',')[3]);
+//        var elftwo = Enumerable.Range(elf2low, (elf2hi - elf2low)+1).ToArray();
+            
+//        if (elfone.Intersect(elftwo).Any())
+//            count++;
+//    }
+//    Console.WriteLine(count);
+//}
