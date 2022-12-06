23390d3a8158f8428e2bb03d4cc23fe14d2dfc1c
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-12-06 12:42:06 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.WriteLine(Part1("input.txt"));
+Console.WriteLine(Part2("input.txt"));
+
+static int Part1(string inputpath)
+{
+    string input = File.ReadAllText(inputpath);
+    try
+    {
+        var recentFourChars = new List<char>
+        {
+            input[0],
+            input[1],
+            input[2]
+        };
+        for (int i = 3; i < input.Length; i++)
+        {
+            recentFourChars.Add(input[i]);
+            if (recentFourChars.Count == recentFourChars.Distinct().Count())
+                return i + 1;
+            else
+                recentFourChars.RemoveAt(0);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    return 0;
+}
+
+static int Part2(string inputpath)
+{
+    string input = File.ReadAllText(inputpath);
+    try
+    {
+        var recentMessage = new List<char>
+        {
+            input[0],
+            input[1],
+            input[2],
+            input[3],
+            input[4],
+            input[5],
+            input[6],
+            input[7],
+            input[8],
+            input[9],
+            input[10],
+            input[11],
+            input[12],
+        };
+        for (int i = 13; i < input.Length; i++)
+        {
+            recentMessage.Add(input[i]);
+            if (recentMessage.Count == recentMessage.Distinct().Count())
+                return i + 1;
+            else
+                recentMessage.RemoveAt(0);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    return 0;
+}
