5d4fe890eca46e51656be841c6188df044336533
Joakim Lindgren <102698651+p1otrboi@users.noreply.github.com>
2022-12-07 23:56:17 +0100

    Day 7

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -84,14 +84,30 @@
 //total sizes is 95437 (94853 + 584). (As in this example, this process can count files more than once!)
 
 //Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
+//---Part Two---
+//Now, you 're ready to choose a directory to delete.
+
+//The total disk space available to the filesystem is 70000000. To run the update, you need unused space of at least 30000000. You need to find a directory you can delete that will free up enough space to run the update.
+
+//In the example above, the total size of the outermost directory (and thus the total amount of used space) is 48381165; this means that the size of the unused space must currently be 21618835, which isn't quite the 30000000 required by the update. Therefore, the update still requires a directory with total size of at least 8381165 to be deleted before it can run.
+
+//To achieve this, you have the following options:
+
+//Delete directory e, which would increase unused space by 584.
+//Delete directory a, which would increase unused space by 94853.
+//Delete directory d, which would increase unused space by 24933642.
+//Delete directory /, which would increase unused space by 48381165.
+//Directories e and a are both too small; deleting them would not free up enough space. However, directories d and / are both big enough! Between these, choose the smallest: d, increasing unused space by 24933642.
+
+//Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update. What is the total size of that directory?
 
 Console.WriteLine(Day7());
-static int Day7()
+static string Day7()
 {
     string[] input = File.ReadAllLines("input.txt");
 
     var root = new Folder("root");
-    var curFolder = root;
+    var currentFolder = root;
 
     for (int i = 0; i < input.Length; i++)
     {
@@ -101,24 +117,24 @@ static int Day7()
             continue;
         if (input[i].StartsWith("$ cd "))
         {
-            var output = input[i].Split(' ');
+            var output = input[i].Split(' ')[2];
 
-            switch (output[2])
+            switch (output)
             {
                 case "/":
-                    curFolder = root;
+                    currentFolder = root;
                     continue;
                 case "..":
-                    curFolder = curFolder.parent;
+                    currentFolder = currentFolder.parent;
                     continue;
                 default:
-                    if (curFolder.subFolders.Exists(x => x.name == output[2]))
-                        curFolder = curFolder.subFolders.First(x => x.name == output[2]);
+                    if (currentFolder.subFolders.Exists(x => x.name == output))
+                        currentFolder = currentFolder.subFolders.First(x => x.name == output);
                     else
                     {
-                        var newFolder = new Folder($"{output[2]}", curFolder);
-                        curFolder.subFolders.Add(newFolder);
-                        curFolder = newFolder;
+                        var newFolder = new Folder($"{output}", currentFolder);
+                        currentFolder.subFolders.Add(newFolder);
+                        currentFolder = newFolder;
                     }
                     continue;
             }
@@ -129,19 +145,15 @@ static int Day7()
 
             if (long.TryParse(output[0], out var result))
             {
-                curFolder.files.Add(output[1], result); ;
+                currentFolder.files.Add(output[1], result); ;
             }
         }
     }
-    return root.GetTotalBelow(100000);
+    var rootSize = root.GetTotalFileSize();
+    var needToFree = -70000000 + 30000000 + rootSize;
+    return $"Part 1: {root.GetTotalBelow(100000)}" +
+        $"\nPart 2: {root.FindDirectoriesAbove(needToFree).Min()}";
 }
-
-
-
-
-
-
-
 
 class Folder
 {
@@ -158,12 +170,52 @@ class Folder
 
     public long GetTotalFileSize()
     {
-        long size = 0;
+        long fileSize = 0;
         foreach (var folder in subFolders)
-            size += folder.GetTotalFileSize();
+            fileSize += folder.GetTotalFileSize();
         foreach (var file in files)
-            size += file.Value;
-        return size;
+            fileSize += file.Value;
+        return fileSize;
+    }
+
+    public long GetTotalBelow(long max)
+    {
+        long value = 0;
+        long folderSize = 0;
+
+        foreach (var folder in subFolders)
+            value += folder.GetTotalBelow(max);
+
+        foreach (var folder in subFolders)
+            folderSize += folder.GetTotalFileSize();
+
+        foreach (var file in files)
+            folderSize += file.Value;
+
+        if (folderSize <= max)
+            value += folderSize;
+
+        return value;
+    }
+
+    public List<long> FindDirectoriesAbove(long minSize)
+    {
+        long folderSize = 0;
+        var folderSizes = new List<long>();
+
+        foreach (var folder in subFolders)
+            folderSizes.AddRange(folder.FindDirectoriesAbove(minSize));
+
+        foreach (var folder in subFolders)
+            folderSize += folder.GetTotalFileSize();
+
+        foreach (var file in files)
+            folderSize += file.Value;
+
+        if (folderSize >= minSize)
+            folderSizes.Add(folderSize);
+
+        return folderSizes;
     }
 
 }
