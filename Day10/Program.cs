a900f1c87756aa877b6e92228941ad0c59fd7758
Joakim Lindgren <102698651+p1otrboi@users.noreply.github.com>
2023-03-08 16:01:05 +0100

    wip

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -12,21 +12,28 @@ var cycle = 0;
 var buffer = 0;
 var addBuffer = false;
 
+// part 2 
+
+var crtScreen = new char[40, 6];
+crtScreen[0, 0] = '#';
+
 
 for (var i = 0; cycle < minCycles; i++)
 {
 	cycle++;
 	addSignalStrength();
+    drawCrtScreen();
 
-	if (input[i] != "noop")
+    if (input[i] != "noop")
 	{
 		buffer = int.Parse(input[i].Split(' ')[1]);
 		addBuffer = true;
 		cycle++;
 		addSignalStrength();
-	}
+        drawCrtScreen();
+    }
 
-	if (addBuffer)
+    if (addBuffer)
 	{
 		register += buffer;
 		addBuffer = false;
@@ -41,3 +48,26 @@ void addSignalStrength()
 }
 
 Console.WriteLine($"Part 1: Sum of the signalstrengths is {signalStrengths.Sum()}");
+
+// part 2
+
+for (var y = 0; y < 6; y++)
+{
+    for (var x = 0; x < 40; x++)
+    {
+        Console.Write(crtScreen[x, y]);
+    }
+    Console.WriteLine();
+}
+
+void drawCrtScreen()
+{
+    var x = (cycle - 1) % 40;
+    var y = cycle / 40;
+
+    if (x < 40 && y < 6)
+        if (Math.Abs(register - x) <= 1)
+            crtScreen[x, y] = '#';
+        else
+            crtScreen[x, y] = '.';
+}
